d792c123744c5d1f2d921c5642acfa0ae53f9582
crishrpz <criszamarco@gmail.com>
2024-09-30 10:27:09 +0200

    fix css

--- app/dashboard/products/[id]/page.jsx
+++ app/dashboard/products/[id]/page.jsx
@@ -8,20 +8,18 @@ const SingleProductPage = async ({ params }) => {
   const product = await fetchProduct(id);
 
   return (
-    <div className="grid lg:grid-cols-2  md:grid-cols-1 justify-items-center w-full h-full p-2 mt-3 ">
-      <div className={styles.infoContainer}>
-        <div className={styles.imgContainer} style={{ position: 'relative', width: '150px', height: '150px' }}>
+    <div className="grid lg:grid-cols-2 justify-items-center w-full h-full p-2 mt-3 ">
+      <div className="h-[200px]  p-4 bg-[#182236] rounded-xl mb-2">
+        <div className="relative w-[150px] h-[150px] rounded-xl">
           {product.imageUrl && product.imageUrl.length > 0 ? (
             <Image
               src={product.imageUrl[0]} // Usa il primo URL nell'array
               alt={product.title || "Product image"}
               fill
-              style={{ objectFit: "cover" }}
-              
-              sizes="(max-width: 768px) 100vw, 50vw"
+              style={{ objectFit: "cover" }}            
+              //sizes="(max-width: 768px) 100vw, 50vw"
             />
-            
-            
+   
           ) : (
             <div>No image available</div>
           )}
@@ -29,7 +27,7 @@ const SingleProductPage = async ({ params }) => {
         {product.title}
       </div>
 
-      <div className="w-full p-4 bg-[#182236] rounded-xl">
+      <div className=" p-4 bg-[#182236] rounded-xl w-full">
         <form action={updateProduct} className={styles.form}>
           <input type="hidden" name="id" value={product.id} />
           <label>Title</label>
